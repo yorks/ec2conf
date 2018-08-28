8bbda4c802f40582976c3cb7e29559856c58df6a
yorks <yorks.yang@163.com>
2018-08-28 12:52:17 +0800

    remove some error msg output

--- ec2-init.sh
+++ ec2-init.sh
@@ -9,6 +9,7 @@ for svr in `cat /root/.init.autostart.list`; do
     [[ "x$svr" == "xsshd.service" ]] && continue
     [[ "x$svr" == "xnetwork.service" ]] && continue
     [[ "x$svr" == "xirqbalance.service" ]] && continue
+    systemctl disable $svr
 done
 
 
@@ -17,7 +18,7 @@ yum -y -q install automake libtool git
 #
 # user
 #
-useradd yorks
+useradd yorks 2>>/dev/null
 cp /etc/skel/.bash* /home/yorks/
 mkdir -p /home/yorks/.ssh
 cat /home/ec2-user/.ssh/authorized_keys  > /home/yorks/.ssh/authorized_keys
@@ -51,20 +52,20 @@ cat /tmp/sshd_config  > /etc/ssh/sshd_config
 /bin/rm -f /tmp/sshd_config
 
 /usr/sbin/sshd -t &&  systemctl  reload sshd
-echo -e "\033[40;31;1;5m pls check login before logout!! \033[0m COOL"
+echo -e "\033[40;31;1;5m pls check login before logout!! \033[0m"
 
 #
 # cron
 #
 sed -i 's/MAILTO=root/MAILTO=""/g' /etc/crontab
 sed -i 's/MAILTO=root/MAILTO=""/g' /etc/cron.d/0hourly
-grep MAIL /etc/cron*
-grep MAIL /etc/cron*/*
+grep MAIL /etc/cron* 2>/dev/null
+grep MAIL /etc/cron*/* 2>/dev/null
 
 
 #
 # my autostart svr
 #
-echo 'su - yorks -c "sh /home/yorks/SS/bin/ssserver.sh"' >> /etc/rc.d/rc.local
-echo '/home/nginx/sbin/nginx' >> /etc/rc.d/rc.local
+grep -q ssserver.sh /etc/rc.d/rc.local || echo 'su - yorks -c "sh /home/yorks/SS/bin/ssserver.sh"' >> /etc/rc.d/rc.local
+grep -q nginx /etc/rc.d/rc.local || echo '/home/nginx/sbin/nginx' >> /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
